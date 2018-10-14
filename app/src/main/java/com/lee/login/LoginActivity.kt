0581ad7ad1275272e8a5d8d82de85615fe8368a8
李晨 <lichen@newhope.cn>
2018-10-14 20:16:09 +0800

    commit

--- app/src/main/java/com/lee/login/LoginActivity.kt
+++ app/src/main/java/com/lee/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.lee.login
 
 import android.os.Bundle
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.framework.base.BaseActivity
 import com.framework.http.model.BaseResponse
@@ -8,6 +9,7 @@ import com.framework.http.utils.ParamsUtils
 import com.framework.listener.RxView
 import com.framework.router.RouterConstants
 import com.framework.utils.DeviceUtils
+import com.framework.utils.LogUtils
 import com.framework.utils.NetworkUtils
 import com.framework.utils.ToastUtils
 import com.lee.R
@@ -41,7 +43,36 @@ class LoginActivity : BaseActivity(), LoginContract.View {
             params.put("terminalId", DeviceUtils.getAndroidID())
             mPresenter.login(ParamsUtils.packParams(params, "member-login", "memberLogin"), this@LoginActivity.bindToLifecycle<BaseResponse<MemberLoginResponse>>())
         })
+        RxView.setOnClick(finger, {
+            val manager = FingerprintManagerCompat.from(this)
+            manager.authenticate(null, 0, null, MyCallBack(), null)
+        })
     }
 
 
+    class MyCallBack : FingerprintManagerCompat.AuthenticationCallback() {
+        private val TAG = "MyCallBack"
+
+        // 当出现错误的时候回调此函数，比如多次尝试都失败了的时候，errString是错误信息
+        override fun onAuthenticationError(errMsgId: Int, errString: CharSequence?) {
+            LogUtils.d(TAG, "onAuthenticationError: $errString")
+        }
+
+        // 当指纹验证失败的时候会回调此函数，失败之后允许多次尝试，失败次数过多会停止响应一段时间然后再停止sensor的工作
+        override fun onAuthenticationFailed() {
+            LogUtils.d(TAG, "onAuthenticationFailed: " + "验证失败");
+            //  Toast.makeText(getApplicationContext(), "指纹错误", Toast.LENGTH_LONG).show();
+        }
+
+        override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence?) {
+            LogUtils.d(TAG, "onAuthenticationHelp: $helpString");
+        }
+
+        // 当验证的指纹成功时会回调此函数，然后不再监听指纹sensor
+        override fun onAuthenticationSucceeded(result: FingerprintManagerCompat.AuthenticationResult?) {
+
+            //Toast.makeText(getApplicationContext(), "识别成功", Toast.LENGTH_LONG).show();
+            LogUtils.d(TAG, "onAuthenticationSucceeded: " + "验证成功")
+        }
+    }
 }
---
+++ app/src/test/java/com/lee/TestActivity.java
@@ -0,0 +1,18 @@
+package com.lee;
+
+import com.framework.base.BaseActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lichen on 2018/8/27.
+ */
+
+public class TestActivity extends BaseActivity {
+
+
+    void test() {
+        new ArrayList<>();
+    }
+
+}
